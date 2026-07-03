774d8699abf269f8d60fa37b2f880631e22da346
SalmanDesaign-er <sekretariat.salmantauhid@gmail.com>
2026-07-03 07:12:26 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH ANGKA INI SETIAP KALI BOS UPDATE index.html (Misal: v2, v3, v4)
-const CACHE_NAME = 'mini-obs-v9.6.6'; 
+const CACHE_NAME = 'mini-obs-v9.6.7'; 
 
 const urlsToCache = [
  './',
